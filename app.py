a3d6c50238ad946ca6deebf74100f8c593ee7f74
sahidirfan884 <sahidirfan884@gmail.com>
2026-07-19 01:46:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 # Open webcam (use 0) or replace with your video path
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("C:\Users\thanh\Downloads\stock-footage-turkey-belek-november-the-first-perspective-the-point-of-view-of-the-cctv-camera-in-the (2).webm")
 
 while True:
     ret, frame = cap.read()
